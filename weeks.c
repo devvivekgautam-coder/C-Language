5acd30e146ee89c1733137b0ab44320660fc42c6
devvivekgautam-coder <dev.vivekgautam@gmail.com>
2025-09-03 14:44:56 +0530

    Adding 2 Programs reverse and weeks finding.

---
+++ weeks.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    int day;
+    printf("Enter The Number Between 1-7 :");
+    scanf("%d", &day);
+
+    switch (day)
+    {
+    case 1:
+        printf("It's Mnnday\n");
+        break;
+
+    case 2:
+        printf("It's Tuesday\n");
+        break;
+
+    case 3:
+        printf("It's Wednesday\n");
+        break;
+    case 4:
+        printf("It's Thursday\n");
+        break;
+    case 5:
+        printf("It's Friday\n");
+        break;
+    case 6:
+        printf("It's Saturday\n");
+        break;
+    case 7:
+        printf("It's Sunday\n");
+        break;
+
+    default:
+        printf("Invalid number. Please enter a number between 1 and 7.\n");
+    }
+
+    return 0;
+}
